d45c3e3192f545b9777e5024fb6450151afa9d4f
JacksonWhite4725 <jawh4725@colorado.edu>
2021-11-02 11:49:08 -0600

    Completed range get and repaired workout get request

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -11,7 +11,7 @@ router.get('/api/workouts', (req, res) => {
             } 
         } 
     }
-    ]).then((data) => {
+    ]).then(workoutdb => {
         res.json(workoutdb)
     }).catch(err => {
         res.status(400).json(err);
@@ -33,6 +33,28 @@ router.put('/api/workouts/:id', (req, res) => {
     Workout.findByIdAndUpdate(req.params.id, { $push: {
         exercises: req.body
     }}).then(workoutdb => {
+        res.json(workoutdb);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+// GET Range of Workouts
+router.get('/api/workouts/range', (req, res) => {
+    let range = new Date();
+    range.setDate(range.getDate() - 7);
+    Workout.aggregate([
+        { $match: {
+            day: {
+                $gt: range
+            }
+        }},
+        { $addFields: {
+            totalDuration: {
+                $sum: '$exercises.duration' 
+            } 
+        }},
+    ]).then(workoutdb => {
         res.json(workoutdb);
     }).catch(err => {
         res.status(400).json(err);
