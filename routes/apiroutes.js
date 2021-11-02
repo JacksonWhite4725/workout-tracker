557eb15c9dfe3dd2d608c776d6fd4cb4747548d4
JacksonWhite4725 <jawh4725@colorado.edu>
2021-11-02 10:27:53 -0600

    Created a GET route for workouts

---
+++ routes/apiroutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { Workout } = require('../models');
+
+// GET Workouts
+router.get('/api/workouts', async (req, res) => {
+    const workouts = await Workout.find();
+    try {
+        res.json(workouts);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
